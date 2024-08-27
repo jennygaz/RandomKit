ccf7b08b0e66aeebaa20773601cc15d42a3a8fc5
jgallegos1 <jgallegos@griddynamics.com>
2024-08-27 16:41:22 -0600

    [ADD] Jumpable protocol, for allowing engines to jump steps with a common interface

---
+++ Sources/Xoshiro/Jumpable.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jenny Gallegos Cardenas on 27/08/24.
+//
+
+public protocol Jumpable {
+    mutating func jump() -> Void
+    mutating func longJump() -> Void
+}
